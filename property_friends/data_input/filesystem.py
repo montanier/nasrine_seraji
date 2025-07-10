d513257566deabaf3098318bd040fef7b945d440
Jean-Marc <jean-marc.montanier@bosch.com>
2025-07-10 23:24:32 +0200

    feat: structure
    Dockerfile, data input, tests, ci

---
+++ property_friends/data_input/filesystem.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def load_data(train_path: str, test_path: str) -> (pd.DataFrame, pd.DataFrame):
+    """
+    Loads the train and test data into pandas DataFrames
+    """
+    train = pd.read_csv(train_path)
+    test = pd.read_csv(test_path)
+    return train, test
---
+++ tests/test_data_input_filesystem.py
@@ -0,0 +1,43 @@
+import pytest
+import pandas as pd
+import tempfile
+import os
+from property_friends.data_input.filesystem import load_data
+
+
+def test_load_data():
+    # Given
+    train_data = pd.DataFrame({
+        'feature1': [1, 2, 3],
+        'feature2': ['a', 'b', 'c'],
+        'target': [10, 20, 30]
+    })
+    
+    test_data = pd.DataFrame({
+        'feature1': [4, 5],
+        'feature2': ['d', 'e']
+    })
+    
+    with tempfile.TemporaryDirectory() as temp_dir:
+        train_path = os.path.join(temp_dir, 'train.csv')
+        test_path = os.path.join(temp_dir, 'test.csv')
+        
+        train_data.to_csv(train_path, index=False)
+        test_data.to_csv(test_path, index=False)
+        
+        # When
+        loaded_train, loaded_test = load_data(train_path, test_path)
+        
+        # Then
+        pd.testing.assert_frame_equal(loaded_train, train_data)
+        pd.testing.assert_frame_equal(loaded_test, test_data)
+
+
+def test_load_data_file_not_found():
+    # Given
+    nonexistent_train_path = 'nonexistent_train.csv'
+    nonexistent_test_path = 'nonexistent_test.csv'
+    
+    # When / Then
+    with pytest.raises(FileNotFoundError):
+        load_data(nonexistent_train_path, nonexistent_test_path)
